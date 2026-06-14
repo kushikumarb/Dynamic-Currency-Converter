04900a1cf4a080b674a2d6f7b71e81067ab2f662
kushikumarb <143270392+kushikumarb@users.noreply.github.com>
2026-06-14 12:32:41 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,57 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
+
+let dropdowns = document.querySelectorAll(".dropdown select")
+let btn = document.querySelector("form button");
+let fromCurr = document.querySelector(".from select");
+let toCurr = document.querySelector(".to select");
+let msg = document.querySelector(".msg")
+
+
+for ( let select of dropdowns){
+    for (currcode in countryList){
+        let newOption = document.createElement("option");
+        newOption.innerText = currcode;
+        newOption.value = currcode;
+        if (select.name === "from" && currcode === "USD"){
+            newOption.selected = "selected";
+        } else if (select.name === "to" && currcode === "INR"){
+            newOption.selected = "selected";
+        }
+        select.append(newOption);
+    }
+    select.addEventListener("change", (evt) =>{
+        updateFlag(evt.target);
+    });
+}
+
+const updateExchangeRate = async ()=>{
+    let amount = document.querySelector(".amount input");
+    let amountValue = amount.value;
+    if (amountValue === "" || amountValue <1){
+        amountValue = 1;
+        amount.value = "1";
+    }
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+    let response  = await fetch(URL);
+    let data = await response.json();
+    let rate = data[fromCurr.value.toLowerCase()][toCurr.value.toLowerCase()]
+    let finalAmt = rate * amountValue;
+    msg.innerText = `${amountValue} ${fromCurr.value} = ${finalAmt} ${toCurr.value}`;
+}
+
+const updateFlag = (element) =>{
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc
+};
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    updateExchangeRate();
+});
+
+window.addEventListener("load", () => {
+    updateExchangeRate();
+});
